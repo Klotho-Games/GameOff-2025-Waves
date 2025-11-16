8cdfb785a5ce003742b046b55982bcbb2ea06604
Konrad Kovacs <112723058+Soko1414KonradKovacs@users.noreply.github.com>
2025-11-16 14:39:52 +0100

    Refactor Highlightable Canvas Scaling

--- Assets/Scripts/MonoBehaviour/RectTransformFullScreenScaler.cs
+++ Assets/Scripts/MonoBehaviour/RectTransformFullScreenScaler.cs
@@ -7,45 +7,20 @@ public class RectTransformFullScreenScaler : MonoBehaviour
     [SerializeField] private Camera cam;
     private RectTransform rect;
 
-    private int lastScreenWidth = 0;
-    private int lastScreenHeight = 0;
-    private float lastCamSize = 0f;
-    private Camera lastCam = null;
-
-    void OnEnable()
+    void Start()
     {
         if (rect == null)
             rect = GetComponent<RectTransform>();
-        Resize();
-        CacheState();
-    }
-
-    void Update()
-    {
         if (cam == null)
             cam = Camera.main;
 
-        // If resolution changed, camera changed, or camera zoom changed, resize
-        if (cam != lastCam || Screen.width != lastScreenWidth || Screen.height != lastScreenHeight || (cam != null && cam.orthographicSize != lastCamSize))
-        {
-            Resize();
-            CacheState();
-        }
+        Resize();
     }
 
     private void OnValidate()
     {
         // Called in editor when serialized fields change
         Resize();
-        CacheState();
-    }
-
-    private void CacheState()
-    {
-        lastScreenWidth = Screen.width;
-        lastScreenHeight = Screen.height;
-        lastCam = cam;
-        lastCamSize = cam != null ? cam.orthographicSize : 0f;
     }
 
 
